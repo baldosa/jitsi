01a7b96eeaa20ad3eaa04e45f19d9e5b5435db57
baldosa <ema@guillen.com.ar>
2023-06-20 14:43:06 -0300

    newer ver

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
 echo "Start installation common packages"
+sudo sudo apt update > /dev/null
 sudo apt install -y gpg gnupg2 nginx curl apt-transport-https > /dev/null
 sudo apt-add-repository universe
-
 sudo apt update > /dev/null
-sudo hostnamectl set-hostname meet.example.com
 
-sudo echo "192.168.33.10  meet.example.com" >> /etc/hosts
+sudo hostnamectl set-hostname jitsi.colmena.network
 
+sudo echo "192.168.33.10  jitsi.colmena.network" >> /etc/hosts
+
+echo "Prosody installation"
+echo deb http://packages.prosody.im/debian $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list > /dev/null
+wget https://prosody.im/files/prosody-debian-packages.key -O- | sudo apt-key add - > /dev/null
+sudo apt update > /dev/null
+apt install lua5.2
+
+echo "Jitsi installation"
 curl https://download.jitsi.org/jitsi-key.gpg.key | sudo sh -c 'gpg --dearmor > /usr/share/keyrings/jitsi-keyring.gpg'
 echo 'deb [signed-by=/usr/share/keyrings/jitsi-keyring.gpg] https://download.jitsi.org stable/' | sudo tee /etc/apt/sources.list.d/jitsi-stable.list > /dev/null
 sudo apt update > /dev/null
